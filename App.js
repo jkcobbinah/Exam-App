adacee991dc3214d667bb7f3ff0ebf73800c51da
jkcobbinah <cj@Abeylewa.local>
2022-12-03 14:12:41 +0000

    New files added

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'expo-dev-client';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Pdf from 'react-native-pdf';
 
 export default function App() {
+  const [buttonText, setButtonText] = useState('Android Development');
+  const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to {buttonText}</Text>
+      <Button
+      title={buttonText}
+      onPress={() => setButtonText("JavaScript React Native")}
+      />
+
+<View style={styles.containerTwo}>
+                <Pdf
+                    source={source}
+                    onLoadComplete={(numberOfPages,filePath) => {
+                        console.log(`Number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages) => {
+                        console.log(`Current page: ${page}`);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    onPressLink={(uri) => {
+                        console.log(`Link pressed: ${uri}`);
+                    }}
+                    style={styles.pdf}/>
+            </View>
     </View>
   );
 }
@@ -17,4 +43,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerTwo: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 25,
+},
+pdf: {
+    flex:1,
+    width:Dimensions.get('window').width,
+    height:Dimensions.get('window').height,
+}
 });
